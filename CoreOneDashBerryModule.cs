165117fa9c3c4130412effa0164dc9635daa4c14
EllaTAS <86128357+ella-TAS@users.noreply.github.com>
2024-01-13 12:19:52 +0100

    Modname setup

---
+++ CoreOneDashBerryModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Celeste.Mod.CoreOneDashBerry {
+    public class CoreOneDashBerryModule : EverestModule {
+        
+        public static CoreOneDashBerryModule Instance;
+
+        public CoreOneDashBerryModule() {
+            Instance = this;
+        }
+
+        public override void Load() {
+        }
+
+        public override void Unload() {
+        }
+
+    }
+}
